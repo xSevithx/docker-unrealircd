cbfd1be9649c58b8e12404717a9ecf56c9d67780
xSevithx <sevithstarr@gmail.com>
2023-12-05 14:31:33 -0600

    Update deploy-unrealirc.sh

--- deploy-unrealirc.sh
+++ deploy-unrealirc.sh
@@ -5,7 +5,7 @@
 #echo $MOTD > ircd.motd
 #echo $RULES > ircd.rules
 
-wget https://www.unrealircd.org/unrealircd${UNREAL_VERSION%%.*}/unrealircd-$UNREAL_VERSION.tar.gz
+wget https://www.unrealircd.org/downloads/unrealircd-$UNREAL_VERSION.tar.gz
 #wget https://www.unrealircd.org/unrealircd${UNREAL_VERSION%%.*}/unrealircd-$UNREAL_VERSION.tar.gz{,.asc}
 #gpg --keyserver keys.gnupg.net --recv-keys 0xA7A21B0A108FF4A9
 #gpg --verify unrealircd-$UNREAL_VERSION.tar.gz.asc unrealircd-$UNREAL_VERSION.tar.gz
